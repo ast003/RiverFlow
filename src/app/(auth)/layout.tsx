88d278790e3af938b021a84367d7e90c9ba91171
Astha Singh <rks.astha003@gmail.com>
2025-06-30 18:42:51 +0530

    fixed errors

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -1,23 +1,29 @@
 "use client";
+import { BackgroundBeams } from "@/components/ui/bakground-beams";
 import { useAuthStore } from "@/store/Auth";
-import { useRouter } from "next/router";
+import { useRouter } from "next/navigation";
 import React from "react";
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   const { session } = useAuthStore();
   const router = useRouter();
+
   React.useEffect(() => {
     if (session) {
       router.push("/");
     }
   }, [session, router]);
+
   if (session) {
     return null;
   }
+
   return (
-    <div className="">
-      <div className="">{children}</div>
+    <div className="relative flex min-h-screen flex-col items-center justify-center py-12">
+      <BackgroundBeams />
+      <div className="relative">{children}</div>
     </div>
   );
 };
+
 export default Layout;
